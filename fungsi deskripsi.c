bd5e4ab3a6cb5b33e63680d48e10c0843d5ab8b5
I Wayan Rama Swara <74579764+RamaSwara042@users.noreply.github.com>
2020-12-01 23:26:51 +0800

    menambahkan comment
    menambahkan comment di setiap bagian deskripsi tempat wisata

--- fungsi deskripsi.c
+++ fungsi deskripsi.c
@@ -9,7 +9,7 @@ void deskripsi();
 
 //Fungsi Deskripsi
 void deskripsi(){
-    if(edu==1){
+    if(edu==1){ //deskripsi untuk tempat wisata edukasi tujuan monumen bajra sandhi, denpasar
         puts ("+-----------------------------------------------------------------------+");
         puts ("|                       DESKRIPSI TEMPAT WISATA                         |");
         puts ("|                   MONUMEN BAJRA SANDHI, DENPASAR                      |");
@@ -21,7 +21,7 @@ void deskripsi(){
         puts ("|  keutuhan Negara Kesatuan Republik Indonesia.                         |");
         puts ("+-----------------------------------------------------------------------+");
     }
-    else if(edu==2){
+    else if(edu==2){ //deskripsi untuk tempat wisata edukasi tujuan taman kertha gosa, klungkung
         puts ("+-----------------------------------------------------------------------+");
         puts ("|                      DESKRIPSI TEMPAT WISATA                          |");
         puts ("|                    TAMAN KERTHA GOSA, KLUNGKUNG                       |");
@@ -33,7 +33,7 @@ void deskripsi(){
         puts ("|  yang dilukiskan di langit-langit paviliun.                           |");
         puts ("+-----------------------------------------------------------------------+");
     }
-    else if(edu==3){
+    else if(edu==3){ //deskripsi untuk tempat wisata edukasi tujuan museum geopark batur, bangli
         puts ("+-----------------------------------------------------------------------+");
         puts ("|                      DESKRIPSI TEMPAT WISATA                          |");
         puts ("|                    MUSEUM GEOPARK BATUR, BANGLI                       |");
@@ -45,7 +45,7 @@ void deskripsi(){
         puts ("|  keanekaragaman wujud budaya (cultural diversity).                    |");
         puts ("+-----------------------------------------------------------------------+");
     }
-    else if(konser==1){
+    else if(konser==1){ //deskripsi untuk tempat wisata konservasi tujuan taman nasional bali barat, jembrana
         puts ("+-----------------------------------------------------------------------+");
         puts ("|                      DESKRIPSI TEMPAT WISATA                          |");
         puts ("|                TAMAN NASIONAL BALI BARAT, JEMBRANA                    |");
@@ -57,7 +57,7 @@ void deskripsi(){
         puts ("|  kan satu-satunya burung endemik Bali, yaitu burung jalak bali.       |");
         puts ("+-----------------------------------------------------------------------+");
     }
-    else if(konser==2){
+    else if(konser==2){ //deskripsi untuk tempat wisata konservasi tujuan kebun raya bedugul, tabanan
         puts ("+-----------------------------------------------------------------------+");
         puts ("|                      DESKRIPSI TEMPAT WISATA                          |");
         puts ("|                    KEBUN RAYA BEDUGUL, TABANAN                        |");
@@ -70,7 +70,7 @@ void deskripsi(){
         puts ("|  gunungan tropika Kawasan Timur Indonesia.                            |");
         puts ("+-----------------------------------------------------------------------+");
     }
-    else if(konser==3){
+    else if(konser==3){ //deskripsi untuk tempat wisata konservasi tujuan bali bird park, gianyar
         puts ("+-----------------------------------------------------------------------+");
         puts ("|                      DESKRIPSI TEMPAT WISATA                          |");
         puts ("|                      BALI BIRD PARK, GIANYAR                          |");
@@ -83,7 +83,7 @@ void deskripsi(){
         puts ("|  dilepas bebas.                                                       |");
         puts ("+-----------------------------------------------------------------------+");
     }
-    else if(pan==1){
+    else if(pan==1){ //deskripsi untuk tempat wisata pantai tujuan pantai kuta, badung
         puts ("+-----------------------------------------------------------------------+");
         puts ("|                      DESKRIPSI TEMPAT WISATA                          |");
         puts ("|                        PANTAI KUTA, BADUNG                            |");
@@ -96,7 +96,7 @@ void deskripsi(){
         puts ("|  pantai ini juga pas bagi para peselancar pemula.                     |");
         puts ("+-----------------------------------------------------------------------+");
     }
-    else if(pan==2){
+    else if(pan==2){ //deskripsi untuk tempat wisata pantai tujuan pantai lovina, buleleng
         puts ("+-----------------------------------------------------------------------+");
         puts ("|                      DESKRIPSI TEMPAT WISATA                          |");
         puts ("|                      PANTAI LOVINA, BULELENG                          |");
@@ -109,7 +109,7 @@ void deskripsi(){
         puts ("|  memancing di tengah laut bersama teman-teman.                        |");
         puts ("+-----------------------------------------------------------------------+");
     }
-    else if(pan==3){
+    else if(pan==3){ //deskripsi untuk tempat wisata pantai tujuan pantai sanur, denpasar
         puts ("+-----------------------------------------------------------------------+");
         puts ("|                      DESKRIPSI TEMPAT WISATA                          |");
         puts ("|                       PANTAI SANUR,DENPASAR                           |");
@@ -122,7 +122,7 @@ void deskripsi(){
         puts ("|  pengalaman liburan dengan budget yang lebih terjangkau.              |");
         puts ("+-----------------------------------------------------------------------+");
     }
-    else if(shop==1){
+    else if(shop==1){ //deskripsi untuk tempat wisata shopping tujuan mall bali galeria, badung
         puts ("+-----------------------------------------------------------------------+");
         puts ("|                      DESKRIPSI TEMPAT WISATA                          |");
         puts ("|                     MALL BALI GALERIA, BADUNG                         |");
@@ -135,7 +135,7 @@ void deskripsi(){
         puts ("|  tujuan populer bagi penduduk lokal dan pengunjung.                   |");
         puts ("+-----------------------------------------------------------------------+");
     }
-    else if(shop==2){
+    else if(shop==2){ //deskripsi untuk tempat wisata shopping tujuan pasar seni sukawati, bangli
         puts ("+-----------------------------------------------------------------------+");
         puts ("|                      DESKRIPSI TEMPAT WISATA                          |");
         puts ("|                    PASAR SENI SUKAWATI, BANGLI                        |");
@@ -148,7 +148,7 @@ void deskripsi(){
         puts ("|  harga yang ditawarkan penjual. Jadi pintar-pintarlah menawar.        |");
         puts ("+-----------------------------------------------------------------------+");
     }
-    else if(shop==3){
+    else if(shop==3){ //deskripsi untuk tempat wisata shopping tujuan pasar kumbasari, denpasar
         puts ("+-----------------------------------------------------------------------+");
         puts ("|                      DESKRIPSI TEMPAT WISATA                          |");
         puts ("|                     PASAR KUMBASARI, DENPASAR                         |");
