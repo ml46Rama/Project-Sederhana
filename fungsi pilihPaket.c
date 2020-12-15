42c483d189877d977c8104c1aeee960595b12457
I Wayan Rama Swara <74579764+RamaSwara042@users.noreply.github.com>
2020-12-15 17:12:42 +0800

    Update and rename fungsi kategori.c to fungsi pilihPaket.c
    Perubahan ini dilakukan agar menciptakan suatu fitur yang lebih beragam dibandingkan sebelumnya

---
+++ fungsi pilihPaket.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//deklarasi variabel
+int paket, bagPak;
+
+//deklarasi fungsi
+void pilihPaket();
+
+//fungsi memilih paket
+void pilihPaket(){
+
+puts ("+-------------------------------------------------------------------+");
+puts ("|                   JENIS PAKET PERJALANAN WISATA                   |");
+puts ("+-------------------------------------------------------------------+");
+puts ("| 1.Paket berdasarkan kabupaten     | 2.Paket berdasarkan kategori  |");
+puts ("|   1.Badung      6.Jembrana        |   1.Edukasi                   |");
+puts ("|   2.Denpasar    7.Karangasem      |   2.Konservasi                |");
+puts ("|   3.Gianyar     8.Tabanan         |   3.Religi                    |");
+puts ("|   4.Klungkung   9.Buleleng Barat  |   4.Pantai                    |");
+puts ("|   5.Bangli      10.Buleleng Timur |   5.Shopping                  |");
+puts ("+-------------------------------------------------------------------+");
+printf("Pilihlah kode paket perjalanan anda : ");
+scanf("%d",&paket); //menyimpan data paket pilihan user
+printf("Pilihlah kode bagian paket sesuai tabel di atas : ");
+scanf("%d", &bagPak); //menyimpan data bagian paket pilihan user
+
+hargaPaket(); //memanggil fungsi hargaPaket
+}
