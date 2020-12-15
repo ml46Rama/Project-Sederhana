cab25038d9de14cd735d72009db178cc720f2d97
intanratnasaridewi <74100614+intanratnasaridewi@users.noreply.github.com>
2020-12-15 18:22:33 +0800

    menambahkan fungsi jarak untuk menghitung banyak bensin yang dipakai

---
+++ fungsi jarak.c
@@ -0,0 +1,19 @@
+feeling input_jarak(feeling fl, program call[], sewa sw)
+{
+    if (sw.ask== 0)
+    {
+        printf("\n anda dapat mengecek di google maps dan sesuaikan dengan kendaraan anda");
+        printf("\n masukkan perkiraan jarak tempat anda sekarang ke tujuan :");
+        scanf("%f", &fl.jarak);
+        printf("\n Kendaraan yang di pakai      :");
+        scanf("%d", &fl.kendaraan);
+        if (fl.kendaraan == 1)
+        {
+            fl.jarak_wisata_mobil = fl.jarak * 2 / 19; //jika memakai mobil
+        }
+        else if (fl.kendaraan == 2)
+        {
+            fl.jarak_wisata_motor = fl.jarak * 2 / 37; //jika memakai motor
+        }
+    }
+}
