16e60c429b29c668ef7998955616ae6857c1c814
CarloAgastya <74575801+CarloAgastya@users.noreply.github.com>
2020-12-15 13:28:19 +0700

    tempatmmakan.c
    menambahkan fungsi harga

--- tempatkuliner.c
+++ tempatkuliner.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
 
-//deklarasi variabel
+
+//deklarasi varabel
 int makan,kabupaten,pilihan;
+float harga;
 
+//deklarasi fungsi
 int tempatMakan();
 
 //********************************************************************//
@@ -22,7 +23,7 @@ int tempatMakan();
 int main(){
   puts    (" \n============================================================");
   puts    (" \n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-  puts    ("\n:                      Daftar Kabupaten                     :"); //menginput pilihan kabupaten
+  puts    ("\n:                      Daftar Kabupaten                     :"); //menginput pilihan Kabupaten
   puts    ("\n <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
   puts    ("\n ============================================================");
   puts    (" Silahkan Pilih daftar Kabupaten                               ");
@@ -43,127 +44,157 @@ int main(){
   return 0;
 }
 
-//menginput tempat makan 
+//menginputkan tempat makan
 int tempatMakan(){
   if(kabupaten==1){
     puts (" ==================================== ");
     puts ("                Badung                ");
     puts (" ==================================== ");
-    puts (" 1. D'Cost                            ");
-    puts (" 2. Plengkung                         ");
-    puts (" 3. Warung Kampung                    ");
-    puts (" 4. Warung Sate Tacik Lukluk          ");
+    puts (" <Nama Restoran>               < Harga >  ");
+    puts (" 1. D'Cost                    Rp. 60.000  ");
+    puts (" 2. Plengkung                 Rp. 100.000 ");
+    puts (" 3. Warung Kampung            Rp. 50.000  ");
+    puts (" 4. Warung Sate Tacik Lukluk  Rp. 75.000  ");
     printf("Ketikkan pilihan anda: ");
     scanf("%d",&makan);
+    printf("Ketikkan harga yang dipilih : ");
+    scanf("%f",&harga);
   }
   else
   if(kabupaten==2){
    puts (" ===================================== ");
    puts ("               Denpasar                ");
    puts (" ===================================== ");
-   puts (" 1. Goemerot                           ");
-   puts (" 2. Warung Wardani                     ");
-   puts (" 3. Jebak Bali Kuliner                 ");
-   puts (" 4. Sate Plecing Arjuna                ");
+   puts (" <Nama Restoran>               < Harga >  ");
+   puts (" 1. Goemerot                  Rp. 75.000  ");
+   puts (" 2. Warung Wardani            Rp. 30.000  ");
+   puts (" 3. Jebak Bali Kuliner        Rp. 75.000  ");
+   puts (" 4. Sate Plecing Arjuna       Rp. 35.000  ");
    printf("Ketikkan pilihan anda: " );
    scanf("%d",&makan );
+   printf("Ketikkan harga yang dipilih : ");
+   scanf("%f",&harga);
   }
   else
   if(kabupaten==3){
   puts (" ====================================== ");
   puts ("                 Gianyar                ");
   puts (" ====================================== ");
-  puts (" 1. The Jungle Resto                    ");
-  puts (" 2. Genius Cafe                         ");
-  puts (" 3. Dharma Giri Restaurant              ");
-  puts (" 4. Ngerodok Resto                      ");
+  puts (" <Nama Restoran>               < Harga >  ");
+  puts (" 1. The Jungle Resto          Rp. 75.000  ");
+  puts (" 2. Genius Cafe               Rp. 100.000 ");
+  puts (" 3. Dharma Giri Restaurant    Rp. 50.000  ");
+  puts (" 4. Ngerodok Resto            Rp. 50.000  ");
   printf("Ketikkan pilihan anda: " );
   scanf("%d",&makan );
+  printf("Ketikkan harga yang dipilih : ");
+  scanf("%f",&harga);
   }
   else
   if(kabupaten==4){
   puts (" ====================================== ");
   puts ("                Klungkung               ");
   puts (" ====================================== ");
-  puts (" 1. Warung Bung Tomo                    ");
-  puts (" 2. Merta Sari Restaurant               ");
-  puts (" 3. Papilas Coffee And Roastery         ");
-  puts (" 4. Warung Jumpu                        ");
+  puts (" <Nama Restoran>                  < Harga >  ");
+  puts (" 1. Warung Bung Tomo             Rp. 50.000  ");
+  puts (" 2. Merta Sari Restaurant        Rp. 100.000 ");
+  puts (" 3. Papilas Coffee And Roastery  Rp. 50.000  ");
+  puts (" 4. Warung Jumpu                 Rp. 50.000  ");
   printf("Ketikkan pilihan anda: " );
   scanf("%d",&makan );
+  printf("Ketikkan harga yang dipilih : ");
+  scanf("%f",&harga);
   }
   else
   if(kabupaten==5){
   puts (" ====================================== ");
   puts ("                 Bangli                 ");
   puts (" ====================================== ");
-  puts (" 1. The Amora Bali                      ");
-  puts (" 2. Warung Abang Sari                   ");
-  puts (" 3. Warung Makan Midori                 ");
-  puts (" 4. Nasi Kajeng                         ");
+  puts (" <Nama Restoran>               < Harga >  ");
+  puts (" 1. The Amora Bali            Rp. 150.000 ");
+  puts (" 2. Warung Abang Sari         Rp. 125.000 ");
+  puts (" 3. Warung Makan Midori       Rp. 100.000 ");
+  puts (" 4. Nasi Kajeng               Rp. 50.000  ");
   printf("Ketikkan pilihan anda: " );
   scanf("%d",&makan );
+  printf("Ketikkan harga yang dipilih : ");
+  scanf("%f",&harga);
   }
   else
   if(kabupaten==6){
   puts (" ====================================== ");
   puts ("                Jembrana                ");
   puts (" ====================================== ");
-  puts (" 1. Rumah Makan Tegal Dadong            ");
-  puts (" 2. Warung Jegeg Bagus                  ");
-  puts (" 3. Kedai Jukung                        ");
-  puts (" 4. Waru Kedai Steak                    ");
+  puts (" <Nama Restoran>               < Harga >  ");
+  puts (" 1. Rumah Makan Tegal Dadong  Rp. 40.000  ");
+  puts (" 2. Warung Jegeg Bagus        Rp. 60.000  ");
+  puts (" 3. Kedai Jukung              Rp. 30.000  ");
+  puts (" 4. Waru Kedai Steak          Rp. 75.000  ");
   printf("Ketikkan pilihan anda: " );
   scanf("%d",&makan );
+  printf("Ketikkan harga yang dipilih : ");
+  scanf("%f",&harga);
   }
   else
   if(kabupaten==7){
   puts (" ====================================== ");
   puts ("               Karangasem               ");
   puts (" ====================================== ");
-  puts (" 1. Bali Asli Restaurant                ");
-  puts (" 2. Pondok Mina                         ");
-  puts (" 3. Warung Biker                        ");
-  puts (" 4. Warung Padang Kecag                 ");
+  puts (" <Nama Restoran>               < Harga >  ");
+  puts (" 1. Bali Asli Restaurant      Rp. 270.000 ");
+  puts (" 2. Pondok Mina               Rp. 90.000  ");
+  puts (" 3. Warung Biker              Rp. 70.000  ");
+  puts (" 4. Warung Padang Kecag       Rp. 80.000  ");
   printf("Ketikkan pilihan anda: " );
   scanf("%d",&makan );
+  printf("Ketikkan harga yang dipilih : ");
+  scanf("%f",&harga);
   }
   else
   if(kabupaten==8){
   puts (" ====================================== ");
   puts ("                Tabanan                 ");
   puts (" ====================================== ");
-  puts (" 1. Warung Tanpa Nama                   ");
-  puts (" 2. Warung Makan Dukuh                  ");
-  puts (" 3. Danke                               ");
-  puts (" 4. Paon Mak Mo                         ");
+  puts (" <Nama Restoran>               < Harga >  ");
+  puts (" 1. Warung Tanpa Nama         Rp. 100.000 ");
+  puts (" 2. Warung Makan Dukuh        Rp. 75.000  ");
+  puts (" 3. Danke                     Rp. 75.000  ");
+  puts (" 4. Paon Mak Mo               Rp. 90.000  ");
   printf("Ketikkan pilihan anda: " );
   scanf("%d",&makan );
+  printf("Ketikkan harga yang dipilih : ");
+  scanf("%f",&harga);
   }
   else
   if(kabupaten==9){
   puts (" ====================================== ");
   puts ("             Buleleng Barat             ");
   puts (" ====================================== ");
-  puts (" 1. Tanjung Alam                        ");
-  puts (" 2. Terrasse du Lac Tamblingan Sari     ");
-  puts (" 3. Warung Bik Juk                      ");
-  puts (" 4. Blayag Buleleng                     ");
+  puts (" <Nama Restoran>                       < Harga >  ");
+  puts (" 1. Tanjung Alam                      Rp. 110.000 ");
+  puts (" 2. Terrasse du Lac Tamblingan Sari   Rp. 150.000 ");
+  puts (" 3. Warung Bik Juk                    Rp. 80.000  ");
+  puts (" 4. Blayag Buleleng                   Rp. 10.000  ");
   printf("Ketikkan pilihan anda: " );
   scanf("%d",&makan );
+  printf("Ketikkan harga yang dipilih : ");
+  scanf("%f",&harga);
   }
   else
   if(kabupaten==10){
   puts (" ====================================== ");
   puts ("             Buleleng Timur             ");
   puts (" ====================================== ");
-  puts (" 1. Rumah Makan Manalagi 2              ");
-  puts (" 2. RM Wina Segara                      ");
-  puts (" 3. Warung Jegeg                        ");
-  puts (" 4. Paon Ummi Singaraja                 ");
+  puts (" <Nama Restoran>               < Harga >  ");
+  puts (" 1. Rumah Makan Manalagi 2    Rp. 75.000  ");
+  puts (" 2. RM Wina Segara            Rp. 75.000  ");
+  puts (" 3. Warung Jegeg              Rp. 60.000  ");
+  puts (" 4. Paon Ummi Singaraja       Rp. 150.000 ");
   printf("Ketikkan pilihan anda: " );
   scanf("%d",&makan );
+  printf("Ketikkan harga yang dipilih : ");
+  scanf("%f",&harga);
   }
-  printf("oke");
-  }
-
+else{ printf(" Input yang anda masukkan salah!");
+ }
+}
