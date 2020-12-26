14d3d371766f1ce3d51c6b965cc76fa57d84a9ed
I Wayan Rama Swara <74579764+RamaSwara042@users.noreply.github.com>
2020-12-26 19:16:29 +0800

    Update fungsi pilihPaket.c
    merevisi kode kabupaten

--- fungsi pilihPaket.c
+++ fungsi pilihPaket.c
@@ -10,16 +10,16 @@ void pilihPaket();
 //fungsi memilih paket
 void pilihPaket(){
 
-puts ("+-------------------------------------------------------------------+");
-puts ("|                   JENIS PAKET PERJALANAN WISATA                   |");
-puts ("+-------------------------------------------------------------------+");
-puts ("| 1.Paket berdasarkan kabupaten     | 2.Paket berdasarkan kategori  |");
-puts ("|   1.Badung      6.Jembrana        |   1.Edukasi                   |");
-puts ("|   2.Denpasar    7.Karangasem      |   2.Konservasi                |");
-puts ("|   3.Gianyar     8.Tabanan         |   3.Religi                    |");
-puts ("|   4.Klungkung   9.Buleleng Barat  |   4.Pantai                    |");
-puts ("|   5.Bangli      10.Buleleng Timur |   5.Shopping                  |");
-puts ("+-------------------------------------------------------------------+");
+puts ("+---------------------------------------------------------------------------+");
+puts ("|                       JENIS PAKET PERJALANAN WISATA                       |");
+puts ("+---------------------------------------------------------------------------+");
+puts ("| 1.Paket berdasarkan kabupaten          | 2.Paket berdasarkan kategori     |");
+puts ("|    1.  Badung      6.  Gianyar         |    1.  Edukasi                   |");
+puts ("|    2.  Denpasar    7.  Karangasem      |    2.  Konservasi                |");
+puts ("|    3.  Tabanan     8.  Bangli          |    3.  Religi                    |");
+puts ("|    4.  Jembrana    9.  Buleleng Barat  |    4.  Pantai                    |");
+puts ("|    5.  Klungkung   10. Buleleng Timur  |    5.  Shopping                  |");
+puts ("+---------------------------------------------------------------------------+");
 printf("Pilihlah kode paket perjalanan anda : ");
 scanf("%d",&paket); //menyimpan data paket pilihan user
 printf("Pilihlah kode bagian paket sesuai tabel di atas : ");
