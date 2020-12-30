941505a04ec87218460e78b5b95842cf92674e8f
CarloAgastya <74575801+CarloAgastya@users.noreply.github.com>
2020-12-30 16:17:12 +0700

    Update choose_your_vacation_fix.c
    menambahkan fungsi guide dan fungsi sewa mobil

--- choose_your_vacation_fix.c
+++ choose_your_vacation_fix.c
@@ -986,9 +986,81 @@ void input_tempatmakan()
     clean();
 }
 
+int int_guide(int jumlah_guide, int jam_guide)
+{
+    proses_menu();
+    point(68, 15);
+    puts("===========================================");
+    point(68, 16);
+    puts("|          PENYEWAAN TOUR GUIDE           |");
+    point(68, 17);
+    puts("===========================================");
+    point(68, 18);
+    printf("Anda ingin berapa tour guide?    : ");
+    scanf("%d", &jumlah_guide);
+    point(68, 19);
+    printf("Anda ingin berapa jam menyewa ?  : ");
+    scanf("%d", &jam_guide);
+
+    jumlah_int = jumlah_guide * jam_guide * 20000;
+
+    return jumlah_int;
+}
+
 int pilihanint()
 {
-   //nyewa mobil
+    proses_menu();
+    point(68, 15);
+    puts("=========================================");
+    point(68, 16);
+    puts("|             DAFTAR MOBIL              |");
+    point(68, 17);
+    puts("=========================================");
+    point(68, 18);
+    puts("|1. Mobil (5 Orang)                     |");
+    point(68, 19);
+    puts("|2. Bis Mini                            |");
+    point(68, 20);
+    puts("|3. Bis Besar                           |");
+    point(68, 21);
+    puts("=========================================");
+    point(68, 22);
+    printf("Ketikkan pilihan anda : ");
+    scanf("%d", &pilihmobil);
+
+    if (pilihmobil == 1)
+    {
+        point(68, 23);
+        printf("Harga int perjam %d", MOBIL1);
+        point(68, 24);
+        printf("Anda mau menyewa mobil berapa jam : ");
+        scanf("%d", &jam);
+        hargaint = jam * MOBIL1;
+    }
+    else if (pilihmobil == 2)
+    {
+        point(68, 23);
+        printf("Harga int perjam %d", MOBIL2);
+        point(68, 24);
+        printf("Anda mau menyewa mobil berapa jam : ");
+        scanf("%d", &jam);
+        hargaint = jam * MOBIL2;
+    }
+    else if (pilihmobil == 3)
+    {
+        point(68, 23);
+        printf("Harga int perjam %d", MOBIL3);
+        point(68, 24);
+        printf("Anda mau menyewa mobil berapa jam : ");
+        scanf("%d", &jam);
+        hargaint = jam * MOBIL3;
+    }
+    else
+    {
+        point(68, 23);
+        printf("Input yang anda masukkan salah!");
+    }
+    clean();
 }
 
 int input_jarak(int jarak, int kendaraan, int bbm)
