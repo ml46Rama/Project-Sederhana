fe6a95799c2c939064849b6ca50eb4111dc55c7c
intanratnasaridewi <74100614+intanratnasaridewi@users.noreply.github.com>
2021-01-07 20:12:50 +0800

    Update menu_hotel.c

--- menu_hotel.c
+++ menu_hotel.c
@@ -1,131 +1,371 @@
-#include <stdio.h>
-#include <stdlib.h>
-int a;                //variabe untuk mewakili apakah user ingin meginap
-int b;                //mewakili user memilih jenis tempat penginapan
-int c;                //mewakili user jika menginputkan berapa banyak org yang akan menginap
-int lama;             //untuk menanyakan berapa lama user menginap
-int harga_penginapan; //jumlah harga total
-void fungsi_hotel(int a);
-void jenis_hotel(int b, int lama, int c);
-int fungsi_homestay(int harga_penginapan, int lama);
-int fungsi_bintang3(int harga_penginapan, int lama);
-int fungsi_bintang5(int harga_penginapan, int lama);
-int fungsi_villa(int harga_penginapan, int lama);
-int tampilkan(int harga_penginapan);
-
-int main()
+void fungsi_hotel()
 {
-    fungsi_hotel(a);
-  
-    return 0;
-}
-void fungsi_hotel(int a)
-{
-    printf("apakah anda ingin menginap di suatu tempat?\n");
-    printf("1.for yes 2. for no");
+    proses_menu();
+    point(65, 15);
+    puts("=======================================================");
+    point(65, 16);
+    puts("|                   PESAN PENGINAPAN                  |");
+    point(65, 17);
+    puts("=======================================================");
+    point(65, 18);
+    printf("|     Apakah anda ingin menginap di suatu tempat?     |");
+    point(65, 19);
+    puts("-------------------------------------------------------");
+    point(65, 20);
+    printf("1.Yes                       ||2. No                    ");
+    point(65, 21);
+    puts("-------------------------------------------------------");
+    point(65, 22);
     scanf("%d", &a);
+    clean();
     if (a == 1)
     {
-        jenis_hotel(b, lama, c);
+        jenis_hotel();
     }
     else
     {
         printf("salah");
     }
 }
-void jenis_hotel(int b, int lama, int c)
-{
-    printf("jenis hotel apa yang anda inginkan  :\n");
-    printf("1.home stay 2. bintang 3 3. bintang more than 3 4. vila");
-    scanf("%d", &b);
 
-    printf("anda ingin menginap berapa lama?");
+int jenis_hotel()
+{
+    proses_menu();
+    int pilihan_penginapan;
+    point(68, 15);
+    puts("=============================================");
+    point(68, 16);
+    puts("|        JENIS PENGINAPAN                   |");
+    point(68, 17);
+    puts("=============================================");
+    point(68, 18);
+    puts("|1. Home Stay                               |");
+    point(68, 19);
+    puts("|2. Hotel bintang 3                         |");
+    point(68, 20);
+    puts("|3. Hotel bintang 5                         |");
+    point(68, 21);
+    puts("|4. Villa                                   |");
+    point(68, 22);
+    puts("=============================================");
+    point(68, 23);
+    printf(" Jenis hotel apa yang anda inginkan ?  :");
+    scanf("%d", &b);
+    point(68, 24);
+    printf(" Anda ingin menginap berapa hari ? : ");
     scanf("%d", &lama);
-    printf("anda ingin pesan untuk berapa orang(kecuali vila)?");
+    point(68, 25);
+    printf(" Bila anda memilih villa, ketikkan 1 atau 2 (1 villa berkapasitas 5 orang)");
+    point(68, 26);
+    printf(" Anda ingin pesan untuk berapa orang (kecuali vila)   : ");
     scanf("%d", &c);
+    clean();
+
     if (b == 1)
     {
-        fungsi_homestay(harga_penginapan, lama);
+        point(72, 15);
+        puts("===============================");
+        point(72, 16);
+        puts("|      DAFTAR HOME STAY       |");
+        point(72, 17);
+        puts("===============================");
+        point(72, 18);
+        puts("|1.Cottage DenAyu             |");
+        point(72, 19);
+        puts("|2.Lembayung Sari             |");
+        point(72, 20);
+        puts("|3.Bakungan diJero            |");
+        point(72, 21);
+        puts("|4.Segara Urip                |");
+        point(72, 22);
+        puts("|5.Milla                      |");
+        point(72, 23);
+        puts("|6.Nassybe                    |");
+        point(72, 24);
+        puts("|7.Ida                        |");
+        point(72, 25);
+        puts("|8.D karang                   |");
+        point(72, 26);
+        puts("|9.Taruna                     |");
+        point(72, 27);
+        puts("|10.Madhasativa               |");
+        point(72, 28);
+        puts("===============================");
+        point(72, 29);
+        printf("Dimana anda ingin menginap? :");
+        scanf("%d", &pilihan_penginapan);
+        if (pilihan_penginapan == 1)
+        {
+            harga_penginapan = lama * 160000 * c;
+        }
+        else if (pilihan_penginapan == 2)
+        {
+            harga_penginapan = lama * 185000 * c;
+        }
+        else if (pilihan_penginapan == 3)
+        {
+            harga_penginapan = lama * 150000 * c;
+        }
+        else if (pilihan_penginapan == 4)
+        {
+            harga_penginapan = lama * 250000 * c;
+        }
+        else if (pilihan_penginapan == 5)
+        {
+            harga_penginapan = lama * 300000 * c;
+        }
+        else if (pilihan_penginapan == 6)
+        {
+            harga_penginapan = lama * 200000 * c;
+        }
+        else if (pilihan_penginapan == 7)
+        {
+            harga_penginapan = lama * 300000 * c;
+        }
+        else if (pilihan_penginapan == 8)
+        {
+            harga_penginapan = lama * 365000 * c;
+        }
+        else if (pilihan_penginapan == 9)
+        {
+            harga_penginapan = lama * 280000 * c;
+        }
+        else if (pilihan_penginapan == 10)
+        {
+            harga_penginapan = lama * 400000 * c;
+        }
     }
     else if (b == 2)
     {
-        fungsi_bintang3(harga_penginapan, lama);
-        //fungsi bintang 3
+        point(72, 15);
+        puts("===================================");
+        point(72, 16);
+        puts("|      DAFTAR HOTEL BINTANG 3     |");
+        point(72, 17);
+        puts("===================================");
+        point(72, 18);
+        puts("|1.Losari                         |");
+        point(72, 19);
+        puts("|2.Quest San                      |");
+        point(72, 20);
+        puts("|3.Puri Taman Sari                |");
+        point(72, 21);
+        puts("|4.The Galuh Pakuntan             |");
+        point(72, 22);
+        puts("|5.The Sunjaya                    |");
+        point(72, 23);
+        puts("|6.Puri Saron                     |");
+        point(72, 24);
+        puts("|7.Camessa Hotel Amed             |");
+        point(72, 25);
+        puts("|8.The Volcania                   |");
+        point(72, 26);
+        puts("|9.Sunia Loka                     |");
+        point(72, 27);
+        puts("|10.Melka Excelsior               |");
+        point(72, 28);
+        puts("===================================");
+        point(72, 29);
+        printf("Dimana anda ingin menginap? :");
+        scanf("%d", &pilihan_penginapan);
+        if (pilihan_penginapan == 1)
+        {
+            harga_penginapan = lama * 165000 * c;
+        }
+        else if (pilihan_penginapan == 2)
+        {
+            harga_penginapan = lama * 285000 * c;
+        }
+        else if (pilihan_penginapan == 3)
+        {
+            harga_penginapan = lama * 315000 * c;
+        }
+        else if (pilihan_penginapan == 4)
+        {
+            harga_penginapan = lama * 305000 * c;
+        }
+        else if (pilihan_penginapan == 5)
+        {
+            harga_penginapan = lama * 185000 * c;
+        }
+        else if (pilihan_penginapan == 6)
+        {
+            harga_penginapan = lama * 150000 * c;
+        }
+        else if (pilihan_penginapan == 7)
+        {
+            harga_penginapan = lama * 290000 * c;
+        }
+        else if (pilihan_penginapan == 8)
+        {
+            harga_penginapan = lama * 550000 * c;
+        }
+        else if (pilihan_penginapan == 9)
+        {
+            harga_penginapan = lama * 610000 * c;
+        }
+        else if (pilihan_penginapan == 10)
+        {
+            harga_penginapan = lama * 880000 * c;
+        }
     }
     else if (b == 3)
     {
-        fungsi_bintang5(harga_penginapan, lama);
+        point(72, 15);
+        puts("=================================");
+        point(72, 16);
+        puts("|     DAFTAR HOTEL BINTANG 5    |");
+        point(72, 17);
+        puts("=================================");
+        point(72, 18);
+        puts("|Aryaduta                       |");
+        point(72, 19);
+        puts("|Grand Mega                     |");
+        point(72, 20);
+        puts("|Umadathu                       |");
+        point(72, 21);
+        puts("|Taman Wana                     |");
+        point(72, 22);
+        puts("|Vision Resort                  |");
+        point(72, 23);
+        puts("|UnudOne                        |");
+        point(72, 24);
+        puts("|Rising Star                    |");
+        point(72, 25);
+        puts("|The Klumpu Bali                |");
+        point(72, 26);
+        puts("|Taman Sari Resort              |");
+        point(72, 27);
+        puts("|Nugraha Lovina                 |");
+        point(72, 28);
+        puts("=================================");
+        point(72, 29);
+        printf("Dimana anda ingin menginap? :");
+        scanf("%d", &pilihan_penginapan);
+        if (pilihan_penginapan == 1)
+        {
+            harga_penginapan = lama * 1000000 * c;
+        }
+        else if (pilihan_penginapan == 2)
+        {
+            harga_penginapan = lama * 1500000 * c;
+        }
+        else if (pilihan_penginapan == 3)
+        {
+            harga_penginapan = lama * 340000 * c;
+        }
+        else if (pilihan_penginapan == 4)
+        {
+            harga_penginapan = lama * 1000000 * c;
+        }
+        else if (pilihan_penginapan == 5)
+        {
+            harga_penginapan = lama * 400000 * c;
+        }
+        else if (pilihan_penginapan == 6)
+        {
+            harga_penginapan = lama * 360000 * c;
+        }
+        else if (pilihan_penginapan == 7)
+        {
+            harga_penginapan = lama * 300000 * c;
+        }
+        else if (pilihan_penginapan == 8)
+        {
+            harga_penginapan = lama * 5000000 * c;
+        }
+        else if (pilihan_penginapan == 9)
+        {
+            harga_penginapan = lama * 1200000 * c;
+        }
+        else if (pilihan_penginapan == 10)
+        {
+            harga_penginapan = lama * 650000 * c;
+        }
     }
-    else if (b == 4)
+    else if (b == 4 && c <= 4)
     {
-        fungsi_villa(harga_penginapan, lama);
+        point(72, 15);
+        puts("==============================");
+        point(72, 16);
+        puts("|         DAFTAR VILLA       |");
+        point(72, 17);
+        puts("==============================");
+        point(72, 18);
+        puts("|De Bharata                  |");
+        point(72, 19);
+        puts("|Banyumas                    |");
+        point(72, 20);
+        puts("|Hati Indah                  |");
+        point(72, 21);
+        puts("|Madewi Surf                 |");
+        point(72, 22);
+        puts("|Karma Loka                  |");
+        point(72, 23);
+        puts("|L'Orange Bali               |");
+        point(72, 24);
+        puts("|Akasa Villa Tulamben        |");
+        point(72, 25);
+        puts("|Asli Bali                   |");
+        point(72, 26);
+        puts("|Amertha                     |");
+        point(72, 27);
+        puts("|Araminth                    |");
+        point(72, 28);
+        puts("==============================");
+        point(72, 29);
+        printf("Dimana anda ingin menginap? :");
+        scanf("%d", &pilihan_penginapan);
+        if (pilihan_penginapan == 1)
+        {
+            harga_penginapan = lama * 1320000;
+        }
+        else if (pilihan_penginapan == 2)
+        {
+            harga_penginapan = lama * 1200000;
+        }
+        else if (pilihan_penginapan == 3)
+        {
+            harga_penginapan = lama * 4000000;
+        }
+        else if (pilihan_penginapan == 4)
+        {
+            harga_penginapan = lama * 420000;
+        }
+        else if (pilihan_penginapan == 5)
+        {
+            harga_penginapan = lama * 235000;
+        }
+        else if (pilihan_penginapan == 6)
+        {
+            harga_penginapan = lama * 1530000;
+        }
+        else if (pilihan_penginapan == 7)
+        {
+            harga_penginapan = lama * 335000;
+        }
+
+        else if (pilihan_penginapan == 8)
+        {
+            harga_penginapan = lama * 1000000;
+        }
+        else if (pilihan_penginapan == 9)
+        {
+            harga_penginapan = lama * 1425000;
+        }
+        else if (pilihan_penginapan == 10)
+        {
+            harga_penginapan = lama * 500000;
+        }
+        else
+        {
+            printf("salah");
+        }
     }
     else
     {
         printf("salah");
     }
-}
-int fungsi_homestay(int harga_penginapan, int lama)
-{
-    printf("Nama Homestay");
-    puts("Cottage DenAyu");
-    puts("Lembayung Sari");
-    puts("Bakungan diJero");
-    puts("Segara Urip");
-    puts("Milla");
-    puts("Nassybe");
-    puts("Ida");
-    puts("D karang");
-    puts("Taruna");
-    puts("Madhasativa");
-    harga_penginapan = lama * 200000 * c;
-    return harga_penginapan;
-}
-int fungsi_bintang3(int harga_penginapan, int lama)
-{
-    printf("Nama Hotel Bintang 3");
-    puts("Losari");
-    puts("Quest San");
-    puts("Puri Taman Sari");
-    puts("The Galuh Pakuntan");
-    puts("The Sunjaya");
-    puts("Puri Saron");
-    puts("Camessa Hotel Amed");
-    puts("The Volcania");
-    puts("Sunia Loka");
-    puts("Melka Excelsior");
-    harga_penginapan = lama * 300000 * c;
-    return harga_penginapan;
-}
-int fungsi_bintang5(int harga_penginapan, int lama)
-{
-    printf("Nama Hotel Bintang 4/5");
-    puts("Aryaduta");
-    puts("Grand Mega");
-    puts("Umadathu");
-    puts("Taman Wana");
-    puts("Vision Resort");
-    puts("UnudOne");
-    puts("Rising Star");
-    puts("The Klumpu Bali");
-    puts("Taman Sari Resort");
-    puts("Nugraha Lovina");
-    harga_penginapan = lama * 400000 * c;
-    return harga_penginapan;
-}
-int fungsi_villa(int harga_penginapan, int lama)
-{
-    printf("Nama Villa");
-    puts("De Bharata");
-    puts("Banyumas");
-    puts("Hati Indah");
-    puts("Madewi Surf");
-    puts("Karma Loka");
-    puts("L'Orange Bali");
-    puts("Akasa Villa Tulamben");
-    puts("Asli Bali");
-    puts("Amertha");
-    puts("Araminth");
-    harga_penginapan = lama * 500000;
-    return harga_penginapan;
+
+    clean();
 }
