dc4bcb1e5ef246f00daed03706d4f9c754ca5f91
I Wayan Rama Swara <74579764+RamaSwara042@users.noreply.github.com>
2021-01-07 21:36:59 +0800

    Update nyewamobil.c

--- nyewamobil.c
+++ nyewamobil.c
@@ -1,6 +1,6 @@
 int pilihanint() //menampilkan penyewaan mobil dan daftar daftar mobil yang telah disediakan
 {
-    proses_menu();
+    proses_menu(); //memanggil fungsi proses menu
     point(68, 15);
     puts("=========================================");
     point(68, 16);
@@ -17,7 +17,7 @@ int pilihanint() //menampilkan penyewaan mobil dan daftar daftar mobil yang tela
     puts("=========================================");
     point(68, 22);
     printf("Ketikkan pilihan anda : ");
-    scanf("%d", &pilihmobil);
+    scanf("%d", &pilihmobil); //menyimpan data pada variabel pilihmobil
 
     if (pilihmobil == 1) //jika memilih mobil (5 orang)
     {
@@ -25,7 +25,7 @@ int pilihanint() //menampilkan penyewaan mobil dan daftar daftar mobil yang tela
         printf("Harga int perjam %d", MOBIL1);
         point(68, 24);
         printf("Anda mau menyewa mobil berapa jam : ");
-        scanf("%d", &jam);
+        scanf("%d", &jam); //menyimpan data pada variabel jam
         hargaint = jam * MOBIL1; //menghitung pembayaran penyewaan mobil (5 orang)
     }
     else if (pilihmobil == 2) //jika memilih bis mini
@@ -34,7 +34,7 @@ int pilihanint() //menampilkan penyewaan mobil dan daftar daftar mobil yang tela
         printf("Harga int perjam %d", MOBIL2);
         point(68, 24);
         printf("Anda mau menyewa mobil berapa jam : ");
-        scanf("%d", &jam);
+        scanf("%d", &jam); //menyimpan data pada variabel jam
         hargaint = jam * MOBIL2; //menghitung pembayaran penyewaan bis mini
     }
     else if (pilihmobil == 3) //jika memilih bis besar
@@ -43,7 +43,7 @@ int pilihanint() //menampilkan penyewaan mobil dan daftar daftar mobil yang tela
         printf("Harga int perjam %d", MOBIL3);
         point(68, 24);
         printf("Anda mau menyewa mobil berapa jam : ");
-        scanf("%d", &jam);
+        scanf("%d", &jam); //menyimpan data pada variabel jam
         hargaint = jam * MOBIL3; //menghitung pembayaran penyewaan bis besar
     }
     else
@@ -51,5 +51,5 @@ int pilihanint() //menampilkan penyewaan mobil dan daftar daftar mobil yang tela
         point(68, 23);
         printf("Input yang anda masukkan salah!");
     }
-    clean();
+    clean(); //membersihkan tampilan layar
 }
