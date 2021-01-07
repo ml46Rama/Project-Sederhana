73e6a99e3237ab5d48a4cedcc0ebbe3c49140d90
intanratnasaridewi <74100614+intanratnasaridewi@users.noreply.github.com>
2021-01-07 20:14:55 +0800

    Update nyewamobil.c

--- nyewamobil.c
+++ nyewamobil.c
@@ -1,95 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
+int pilihanint()
+{
+    proses_menu();
+    point(68, 15);
+    puts("=========================================");
+    point(68, 16);
+    puts("|             DAFTAR MOBIL              |");
+    point(68, 17);
+    puts("=========================================");
+    point(68, 18);
+    puts("|1. Mobil (5 Orang)                     |");
+    point(68, 19);
+    puts("|2. Bis Mini                            |");
+    point(68, 20);
+    puts("|3. Bis Besar                           |");
+    point(68, 21);
+    puts("=========================================");
+    point(68, 22);
+    printf("Ketikkan pilihan anda : ");
+    scanf("%d", &pilihmobil);
 
-#define MOBIL1 50000 //harga sewa mobil isi 5 orang
-#define MOBIL2 70000 //harga sewa bus mini
-#define MOBIL3 100000//harga sewa bus besar
-
-//********************************************************************//
-//***********             Fungsi nyewamobil            ***************//
-//====================================================================//
-// Nama Fungsi: sewa                                                  //
-// Input Argumen : sewa,main                                          //
-// Output Argumen: sewa                                               //
-// Deskripsi     : Menginput jamsewa, nyewamobil, pilihansewa         //
-// Versi : 1.0                                      Rev. 0            //
-// Tgl: 15-12-2020                                  Tgl: 15-12-2020   //
-// I Gede Carlo Kresnandaka Agastya-2005551053      -                 //
-//====================================================================//
-typedef struct{                //nama struck
-  int ask,pilihmobil,jam;
-  float hargasewa;
-}sewa;
-
-//fungsi jam sewa mobil isi 5 orang
-sewa jamsewa1(sewa sw){
-  printf("\nAnda mau menyewa mobil berapa jam: ");
-  scanf("%d", &sw.jam);
-
-    sw.hargasewa=sw.jam*MOBIL1;
-
-  return sw;
-}
-//fungsi jam sewa bus mini
-sewa jamsewa2(sewa sw){
-  printf("\nAnda mau menyewa mobil berapa jam: ");
-  scanf("%d", &sw.jam);
-
-    sw.hargasewa=sw.jam*MOBIL2;
-
-  return sw;
-}
-//fungsi jam sewa bus besar
-sewa jamsewa3(sewa sw){
-  printf("\nAnda mau menyewa mobil berapa jam: ");
-  scanf("%d", &sw.jam);
-
-    sw.hargasewa=sw.jam*MOBIL3;
-
-  return sw;
-}
-
-//fungsi memilih mobil yang akan disewa
-sewa pilihansewa(sewa sw){
- printf("\n1. mobil isi 5 orang\n 2. Bis Mini\n 3. Bis Besar");
- printf("\nketikkan pilihan anda: ");
- scanf("%d",&sw.pilihmobil);
- if (sw.pilihmobil==1){
-printf("Harga sewa perjam %d", MOBIL1);
-jamsewa1(sw);
-}else if (sw.pilihmobil==2){
-printf("Harga sewa perjam %d", MOBIL2);
-jamsewa2(sw);
-}else if (sw.pilihmobil==3){
-printf("Harga sewa perjam %d", MOBIL3);
-jamsewa3(sw);
-}else{
-printf("Input yang anda masukkan salah!");
-}
-}
-
-//fungsi memilih ingin menyewa mobil
-sewa nyewamobil(sewa sw){
-
-printf("\nApakah anda ingin menyewa mobil?");
-printf("\n0(NO) | 1(YES)");
-printf("\nMasukkan pilihan anda : ");
-scanf("%d", &sw.ask);
-  if (sw.ask==0){
-    //masuk fungsi jarak
-
-  } else if (sw.ask==1){
-    pilihansewa(sw);
-  }else{
-    printf("Input yang anda masukkan salah!");
-  }
-}
-int main(){
-  sewa sw;
-  sw=nyewamobil(sw);
-  sw=pilihansewa(sw);
-  sw=jamsewa1(sw);
-  sw=jamsewa2(sw);
-  sw=jamsewa3(sw);
-  return 0;
+    if (pilihmobil == 1)
+    {
+        point(68, 23);
+        printf("Harga int perjam %d", MOBIL1);
+        point(68, 24);
+        printf("Anda mau menyewa mobil berapa jam : ");
+        scanf("%d", &jam);
+        hargaint = jam * MOBIL1;
+    }
+    else if (pilihmobil == 2)
+    {
+        point(68, 23);
+        printf("Harga int perjam %d", MOBIL2);
+        point(68, 24);
+        printf("Anda mau menyewa mobil berapa jam : ");
+        scanf("%d", &jam);
+        hargaint = jam * MOBIL2;
+    }
+    else if (pilihmobil == 3)
+    {
+        point(68, 23);
+        printf("Harga int perjam %d", MOBIL3);
+        point(68, 24);
+        printf("Anda mau menyewa mobil berapa jam : ");
+        scanf("%d", &jam);
+        hargaint = jam * MOBIL3;
+    }
+    else
+    {
+        point(68, 23);
+        printf("Input yang anda masukkan salah!");
+    }
+    clean();
 }
