904ea8b57a727163584d06a26c88d175e5cb03d2
CarloAgastya <74575801+CarloAgastya@users.noreply.github.com>
2021-01-07 19:53:52 +0700

    Update tampilkan_output.c

--- tampilkan_output.c
+++ tampilkan_output.c
@@ -1,35 +1,33 @@
-int tampilkan_data(sign_up mhs)
+int tampilkan_data(sign_up mhs) //menampilkan data yang telah user input
 {
     int w;
 
-    point(65, 15);
-    puts("======================================");
-    point(65, 16);
-    puts("|            TAMPILAN DATA           |");
-    point(65, 17);
-    puts("======================================");
-    point(65, 18);
-    printf("|Tempat Makan          = %d        ", harga);
-    point(65, 19);
-    printf("|Menyewa Guide         = %d        ", jumlah_int);
-    point(65, 20);
-    printf("|Menyewa Mobil         = %d        ", hargaint);
-    point(65, 21);
-    printf("|Estimasi Bensin       = %d        ", bensin);
-    point(65, 22);
-    printf("|Penginapan            = %d        ", harga_penginapan);
-    point(65, 23);
-    printf("|Tiket                 = %d        ", sumtot);
-    point(65, 24);
-    puts("=====================================|");
-    point(65, 25);
-    printf("Siapkan Budget Sebesar = %d", harga + jumlah_int + hargaint + bensin + harga_penginapan + sumtot);
-    point(65, 30);
-    puts("=====================================|");
-    point(65, 31);
-    printf(" Apakah anda ingin kembali ke bagian menu sebelumnya ? ");
-    point(65, 32);
-    printf(" Ketik (1) Jika IYA | Ketik (2) Jika SELESAI : ");
+    point(75, 15);
+    puts("========================");
+    point(75, 16);
+    puts("|     TAMPILAN DATA    |");
+    point(75, 17);
+    puts("========================");
+    point(75, 18);
+    printf("|Tempat Makan      = %d", harga);
+    point(75, 19);
+    printf("|Menyewa Guide     = %d", jumlah_int);
+    point(75, 20);
+    printf("|Menyewa Mobil     = %d", hargaint);
+    point(75, 21);
+    printf("|Estimasi Bensin   = %d", bensin);
+    point(75, 22);
+    printf("|Penginapan       = %d", harga_penginapan);
+    point(75, 23);
+    printf("|Tiket            = %d", sumtot);
+    point(75, 24);
+    puts("========================");
+    point(75, 25);
+    printf("Total               = %d", harga + jumlah_int + hargaint + bensin + harga_penginapan + sumtot);
+    point(75, 26);
+    printf("ingin kembali ke menu    ? ");
+    point(75, 27);
+    printf("1 jika iya|2 jika mengakhiri     :");
     scanf("%d", &w);
 
     if (w == 1)
@@ -45,8 +43,8 @@ int tampilkan_data(sign_up mhs)
     }
     else
     {
-        point(65, 34);
-        printf(" Kode yang anda masukkan tidak sesuai");
+        point(75, 29);
+        printf("anda memasukkan kode yang salah");
     }
     clean();
     return 0;
